9550b532fcf697534ebfda7371d099c9baba05b7
gilbertruizd <maite.6037@gmail.com>
2024-03-25 12:02:16 -0600

    CRUD de Bodegas

--- SistemaInventarios.Utilidades/DS.cs
+++ SistemaInventarios.Utilidades/DS.cs
@@ -8,5 +8,7 @@ namespace SistemaInventarios.Utilidades
 {
     public static class DS
     {
+        public const string Exitosa = "Exitosa";
+        public const string Error = "Error";
     }
 }
