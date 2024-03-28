7b9614f352032caf21f87c710e02a685a69a9ebb
gilbertruizd <maite.6037@gmail.com>
2024-03-27 18:29:04 -0600

    CRUD de Categoria

--- SistemaInventario.AccesoDatos/Migrations/20240325194859_AgregarCategoriaMigracion.Designer.cs
+++ SistemaInventario.AccesoDatos/Migrations/20240325194859_AgregarCategoriaMigracion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SintemaInventario.AccesoDatos.Data;
 
@@ -11,9 +12,11 @@ using SintemaInventario.AccesoDatos.Data;
 namespace SistemaInventario.AccesoDatos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240325194859_AgregarCategoriaMigracion")]
+    partial class AgregarCategoriaMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
